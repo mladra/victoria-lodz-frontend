687d1cdb63d364d0ae02728f97267ee3444982e0
mladra <michal.ladra.95@gmail.com>
2020-09-18 01:07:13 +0200

    Updates

---
+++ precache-manifest.f276964e4d69b6aa28e8670c5a69d572.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb0f3c50ba2ac207bc19fc8680a7280",
+    "url": "/victoria-lodz-frontend/index.html"
+  },
+  {
+    "revision": "234df8b65550788d829a",
+    "url": "/victoria-lodz-frontend/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "217ecf83ff415fd99ab7",
+    "url": "/victoria-lodz-frontend/static/css/main.d98d7437.chunk.css"
+  },
+  {
+    "revision": "234df8b65550788d829a",
+    "url": "/victoria-lodz-frontend/static/js/2.b24fa338.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/victoria-lodz-frontend/static/js/2.b24fa338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217ecf83ff415fd99ab7",
+    "url": "/victoria-lodz-frontend/static/js/main.dbba4836.chunk.js"
+  },
+  {
+    "revision": "c89f49b7724c3fa2835f",
+    "url": "/victoria-lodz-frontend/static/js/runtime-main.1270a86d.js"
+  },
+  {
+    "revision": "b63dfc4525add5cff074c1d230dbb36b",
+    "url": "/victoria-lodz-frontend/static/media/home-bq.b63dfc45.jpg"
+  },
+  {
+    "revision": "e5c578e1bdbdb54ae53de9fa34ec67d5",
+    "url": "/victoria-lodz-frontend/static/media/logo.e5c578e1.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/victoria-lodz-frontend/precache-manifest.f276964e4d69b6aa28e8670c5a69d572.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/victoria-lodz-frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
