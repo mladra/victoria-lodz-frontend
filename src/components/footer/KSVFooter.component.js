58918cd15c259889274ebdb13f8a7605dd55071a
mladra <michal.ladra.95@gmail.com>
2020-09-18 20:50:06 +0200

    Added displaying labels of icons in footer only on larger screens.

--- src/components/footer/KSVFooter.component.js
+++ src/components/footer/KSVFooter.component.js
@@ -3,19 +3,22 @@ import { EnvelopeFill, GeoAlt } from "react-bootstrap-icons";
 import "./KSVFooter.css";
 import fbLogo from "../../img/fb-logo-72.png";
 
+const GOOGLE_MAPS_LINK = "https://www.google.com/maps/dir/?api=1&amp;destination=51.71454%2C19.47813&amp;fbclid=IwAR1XPypGQD1DJ7yEzRsL_WFAiMEZSzMT1YLxLA4fv_wT-ugkF_Cbbyw7zvs";
+const FB_LINK = "https://www.facebook.com/victorialodz1996";
+
 const KSVFooter = () => {
     return (
         <footer className="footer justify-content-between p-5 d-flex mt-auto align-items-center">
             <div>
-                <EnvelopeFill size="24" />{' '}<a href="mailto:biuro@victorialodz.pl">biuro@victorialodz.pl</a>
+                <EnvelopeFill size="24" />{' '}<a className="d-none d-md-inline" href="mailto:biuro@victorialodz.pl">biuro@victorialodz.pl</a>
             </div>
             <div>
-                <img src={fbLogo} width="24" height="24" alt="" />{' '}<a href="https://www.facebook.com/victorialodz1996">Odwiedź nas na Facebooku!</a>
+                <img src={fbLogo} width="24" height="24" alt="" />{' '}<a className="d-none d-md-inline" href={FB_LINK}>Odwiedź nas na Facebooku!</a>
             </div>
             <div>
-                <GeoAlt size="24" />{' '}
-                <a rel="noreferrer noopener" tabIndex="0" target="_blank" href="https://www.google.com/maps/dir/?api=1&amp;destination=51.71454%2C19.47813&amp;fbclid=IwAR1XPypGQD1DJ7yEzRsL_WFAiMEZSzMT1YLxLA4fv_wT-ugkF_Cbbyw7zvs">
-                    <span>Kosynierów Gdyńskich 18, 93-357 Łódź</span>
+                <GeoAlt size="24"/>{' '}
+                <a className="d-none d-md-inline" rel="noreferrer noopener" tabIndex="0" target="_blank" href={GOOGLE_MAPS_LINK}>
+                    Kosynierów Gdyńskich 18, 93-357 Łódź
                 </a>
             </div>
         </footer>
